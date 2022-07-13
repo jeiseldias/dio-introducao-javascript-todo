f9c0c9e8b4196fde9de4518dfc87685168aa2cc7
Jeisel <jeisel@gmail.com>
2022-07-13 15:32:43 -0300

    Implementando a todo list

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+var adicionar = document.getElementById("btnAdicionar");
+var items = document.getElementsByTagName("li");
+
+adicionar.addEventListener("click", newTask);
+
+function newTask() {
+    let title = document.getElementById("txtTask").value;
+    let item = document.createElement('li');
+    let check = document.createElement('input');
+    let span = document.createElement('span');
+
+    check.setAttribute("type", "checkbox");
+    check.setAttribute("value", items.length);
+    check.addEventListener("change", handleTask);
+
+    span.setAttribute("id", items.length);
+    span.appendChild(document.createTextNode(title));
+    
+    item.appendChild(check);
+    item.appendChild(span);
+
+    document.querySelector('ul').appendChild(item);
+}
+
+function handleTask() {
+    let span = document.getElementById(this.value);
+
+    if(this.checked) {
+        span.style.textDecoration = "line-through";
+    } else {
+        span.style.textDecoration = "none";
+    }
+}
